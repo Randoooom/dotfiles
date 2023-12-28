feda8c1c32a6e760bad27db58b4de4ccf2e69d4d
Fritz Leo Ochsmann <53190645+Randoooom@users.noreply.github.com>
2023-12-28 18:43:59 +0100

    migrate to sway and wayland

--- dot_config/i3/executable_ac.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-STATUS=$(cat /sys/class/power_supply/BAT0/status)
-
-if [ "$STATUS" = "Charging" ]; then
-  $@
-fi
--- dot_config/waybar/executable_nordvpn.sh
+++ dot_config/waybar/executable_nordvpn.sh
@@ -4,7 +4,8 @@ STATUS=$(sudo nordvpn status | grep Status | cut -d ' ' -f6)
 PING=$(ping 1.1.1.1 -4c2 -i0.1 -U | grep min/avg/max/mdev | cut -d ' ' -f4 | cut -d "/" -f2)
 
 if [ "$STATUS" = "Connected" ]; then
-    echo " %{F#8fbcbb}%{T7} Connected%{A}%{F-}%{T-} | $PING ms"
+    CITY=$(sudo nordvpn status | grep City | cut -d ' ' -f2)
+    echo "  $CITY | $PING ms"
 else
-    echo "%{F#fff}%{T7}not connected%{T-}%{A}%{F-} | $PING ms"
+    echo "not connected | $PING ms"
 fi
