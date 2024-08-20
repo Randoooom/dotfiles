fbcda1e14176eed23c054c758246cad2bbfd1273
Randoooom <53190645+Randoooom@users.noreply.github.com>
2024-08-20 21:03:53 +0200

    refactor: switch to zsh and enforce btop

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -3,50 +3,51 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 local servers = {
-	"rust_analyzer",
-	"taplo",
-	"sqlls",
-	"tsserver",
-	"jsonls",
-	"cssls",
-	"bashls",
-	"volar",
-	"bufls",
-	"terraformls",
-	"helm_ls",
-	"pylyzer",
+  "rust_analyzer",
+  "taplo",
+  "sqlls",
+  "tsserver",
+  "jsonls",
+  "cssls",
+  "bashls",
+  "volar",
+  "bufls",
+  "terraformls",
+  "helm_ls",
+  "pylyzer",
+  "jdtls",
 }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		on_init = on_init,
-		capabilities = capabilities,
-	})
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
 end
 
-lspconfig.groovyls.setup({
-	on_attach = on_attach,
-	on_init = on_init,
-	capabilities = capabilities,
-	cmd = { "/home/froc/.local/share/nvim/mason/packages/groovy-language-server/groovy-language-server" },
-})
+lspconfig.groovyls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  cmd = { "/home/froc/.local/share/nvim/mason/packages/groovy-language-server/groovy-language-server" },
+}
 
-lspconfig.yamlls.setup({
-	on_attach = on_attach,
-	on_init = on_init,
-	capabilities = capabilities,
-	settings = {
-		yaml = {
-			format = {
-				enable = true,
-			},
-			validate = true,
-			hover = true,
-			completion = true,
-		},
-	},
-})
+lspconfig.yamlls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      format = {
+        enable = true,
+      },
+      validate = true,
+      hover = true,
+      completion = true,
+    },
+  },
+}
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,172 +1,170 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>fm",
-				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
-				end,
-				mode = "",
-				desc = "Format buffer",
-			},
-		},
-		config = function()
-			require("configs.conform")
-		end,
-		init = function()
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>fm",
+        function()
+          require("conform").format { async = true, lsp_format = "fallback" }
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    config = function()
+      require "configs.conform"
+    end,
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("nvchad.configs.lspconfig").defaults()
-			require("configs.lspconfig")
-		end,
-	},
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
 
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua-language-server",
-				"stylua",
-				"css-lsp",
-				"html-lsp",
-				"typescript-language-server",
-				"deno",
-				"prettier",
-				"clangd",
-				"clang-format",
-				"rust-analyzer",
-				"taplo",
-				"sqlls",
-				"groovy-language-server",
-				"yaml-language-server",
-				"npm-groovy-lint",
-				"helm-ls",
-				"terraform-ls",
-				"yamlfmt",
-				"vue-language-server",
-				"pylyzer",
-			},
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "prettier",
+        "clangd",
+        "clang-format",
+        "rust-analyzer",
+        "taplo",
+        "sqlls",
+        "groovy-language-server",
+        "yaml-language-server",
+        "npm-groovy-lint",
+        "helm-ls",
+        "terraform-ls",
+        "yamlfmt",
+        "vue-language-server",
+        "pylyzer",
+        "jdtls",
+      },
+    },
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"vim",
-				"lua",
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"tsx",
-				"c",
-				"markdown",
-				"markdown_inline",
-				"rust",
-				"sql",
-				"vue",
-				"terraform",
-				"toml",
-			},
-			endwise = {
-				enable = true,
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "c",
+        "markdown",
+        "markdown_inline",
+        "rust",
+        "sql",
+        "vue",
+        "terraform",
+        "toml",
+        "java",
+      },
+      endwise = {
+        enable = true,
+      },
+    },
+  },
 
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^3",
-		ft = { "rust" },
-	},
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    ft = { "rust" },
+  },
 
-	{
-		"saecki/crates.nvim",
-		event = { "BufRead Cargo.toml" },
-		config = function()
-			require("crates").setup({
-				completion = {
-					cmp = {
-						enabled = true,
-					},
-				},
-			})
-		end,
-	},
+  {
+    "saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true,
+          },
+        },
+      }
+    end,
+  },
 
-	{
-		"https://git.sr.ht/~reggie/licenses.nvim",
-		lazy = false,
-		config = function()
-			require("licenses").setup({ license = "MIT", copyright_holder = "OneLiteFeatherNet" })
-			require("telescope").load_extension("licenses-nvim")
-		end,
-	},
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
 
-	{
-		"wakatime/vim-wakatime",
-		lazy = false,
-	},
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+  {
+    "towolf/vim-helm",
+    ft = { "yaml" },
+  },
 
-	{
-		"towolf/vim-helm",
-		ft = { "yaml" },
-	},
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = { "Neogit" },
+    config = function()
+      require("neogit").setup {
+        commit_editor = {
+          kind = "split",
+        },
+        log_view = {
+          kind = "split",
+        },
+        integrations = {
+          telescope = true,
+          diffview = true,
+        },
+      }
+    end,
+  },
 
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		cmd = { "Neogit" },
-		config = function()
-			require("neogit").setup({
-				commit_editor = {
-					kind = "split",
-				},
-				log_view = {
-					kind = "split",
-				},
-				integrations = {
-					telescope = true,
-					diffview = true,
-				},
-			})
-		end,
-	},
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+    },
+  },
 
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			current_line_blame = true,
-		},
-	},
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
 
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup()
-		end,
-	},
+  {
+    "mfussenegger/nvim-jdtls",
+    ft = { "java" },
+  },
 }
